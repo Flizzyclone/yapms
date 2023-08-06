d6f476d5d856cd82921ba103175118cdfc29bafc
Flizzyclone <stormry20@gmail.com>
2023-08-06 17:44:46 -0400

    Split state patterns

--- apps/yapms/src/lib/stores/regions/Regions.ts
+++ apps/yapms/src/lib/stores/regions/Regions.ts
@@ -1,10 +1,11 @@
 import type Region from '$lib/types/Region';
-import { calculateLumaHEX } from '$lib/utils/luma';
+import { blendHexes, calculateLumaHEX } from '$lib/utils/luma';
 import { derived, writable, get } from 'svelte/store';
 import { TossupCandidateStore, CandidatesStore } from '../Candidates';
 import { ModeStore } from '../Mode';
 import { disableRegion, editRegion, fillRegion, lockRegion, splitRegion } from './regionActions';
 import { InteractionStore } from '../Interaction';
+import { browser } from '$app/environment';
 
 /**
  * Stores the state of all regions.
@@ -16,28 +17,12 @@ export const RegionsStore = writable<Region[]>([]);
   update the colors of the regions in the DOM
 */
 RegionsStore.subscribe((regions) => {
-	// reduce extra counts
+	if (browser) {
+		const patterns = document?.getElementById("map-div")?.querySelector('svg')?.querySelectorAll('pattern');
+		patterns?.forEach((pattern) => pattern.remove());
+	}
+	
 	regions.forEach((region) => {
-		// get the winner of the district
-		const winner = region.disabled
-			? {
-					candidate: get(TossupCandidateStore),
-					count: 0,
-					margin: 0
-			  }
-			: region.candidates.reduce(
-					(prev, current) => (prev.count > current.count ? prev : current),
-					region.candidates[0]
-			  );
-
-		// set the margin of the new winner
-		let marginIndex = winner.margin ?? 0;
-		if (marginIndex >= winner.candidate.margins.length) {
-			marginIndex = winner.candidate.margins.length - 1;
-		} else if (marginIndex < 0) {
-			marginIndex = 0;
-		}
-
 		// reduce extra counts
 		let totalVotes = region.candidates.reduce(
 			(totalVotes, candidate) => totalVotes + candidate.count,
@@ -61,19 +46,72 @@ RegionsStore.subscribe((regions) => {
 			}
 		}
 
-		region.nodes.region.style.fill = winner.candidate.margins[marginIndex]?.color;
+		// get the winner(s) of the district
+		const maxValue = region.candidates.reduce(
+			(prev, current) => (prev > current.count ? prev : current.count),
+			region.candidates[0].count
+	  	);
+		const winners = region.disabled
+			? [{
+					candidate: get(TossupCandidateStore),
+					count: 0,
+					margin: 0
+			  }]
+			: region.candidates.filter((candidate) => candidate.count === maxValue);
+
+
+		// set the margin of the new winner
+		let marginIndex = winners[0].margin ?? 0;
+		if (marginIndex >= winners[0].candidate.margins.length) {
+			marginIndex = winners[0].candidate.margins.length - 1;
+		} else if (marginIndex < 0) {
+			marginIndex = 0;
+		}
+
+		let fill = '';
+		let lumaColor = '';
+		if (winners.length === 1) {
+			fill = winners[0].candidate.margins[marginIndex]?.color
+			lumaColor = fill;
+		} else {
+			const pattern = document.createElementNS('http://www.w3.org/2000/svg', 'pattern');
+			pattern.setAttribute('patternUnits','userSpaceOnUse');
+			pattern.setAttribute('width',`${10*winners.length}`);
+			pattern.setAttribute('height','10')
+			pattern.setAttribute('patternTransform','rotate(45)')
+			let name = 'repeat';
+			winners.forEach((winner,i: number) => {
+				const line = document.createElementNS('http://www.w3.org/2000/svg','line')
+				line.setAttribute('stroke',winner.candidate.margins[0].color);
+				line.setAttribute('x1',`${5+(10*i)}`);
+				line.setAttribute('x2',`${5+(10*i)}`);
+				line.setAttribute('y1','0');
+				line.setAttribute('y2','10');
+				line.setAttribute('stroke-width',`10`);
+				pattern.appendChild(line);
+				name += `-${winner.candidate.id}`;
+			})
+			pattern.setAttribute('id',name);
+			const mapSVG = document?.getElementById("map-div")?.querySelector('svg');
+			mapSVG?.appendChild(pattern);
+			fill = `url(#${name})`;
+			const colors = winners.map((winner) => winner.candidate.margins[0].color);
+			lumaColor = `#${blendHexes(colors)}`;
+		}
+
+		region.nodes.region.style.fill = fill;
 		region.disabled || region.locked || region.permaLocked
 			? (region.nodes.region.style.fillOpacity = '0.25')
 			: (region.nodes.region.style.fillOpacity = '1'); //Transparent if disabled
 		if (region.nodes.button) {
-			region.nodes.button.style.fill = winner.candidate.margins[marginIndex]?.color;
+			region.nodes.button.style.fill = fill;
 			region.disabled || region.locked || region.permaLocked
 				? (region.nodes.button.style.fillOpacity = '0.25')
 				: (region.nodes.button.style.fillOpacity = '1'); //Transparent if disabled
 		}
 		if (region.nodes.text) {
 			region.nodes.text.style.color =
-				calculateLumaHEX(winner.candidate.margins[marginIndex]?.color) > 0.5 ? 'black' : 'white';
+				calculateLumaHEX(lumaColor) > 0.5 ? 'black' : 'white';
 			const valueText = region.nodes.text.querySelector('[value-text]');
 			if (valueText) {
 				valueText.innerHTML = region.value.toString();
