1a6dfefe54560e3018adcfccb30f624ae7ce7a32
Flizzyclone <stormry20@gmail.com>
2025-06-28 12:18:21 -0400

    this idea is ded

--- apps/yapms/src/lib/components/leaflet/Leaflet.svelte
+++ apps/yapms/src/lib/components/leaflet/Leaflet.svelte
@@ -14,9 +14,9 @@
         //map.setView([-41.47207765, 146.15880365], 7.5);
         //set base params
         map.fitBounds([
-            [ -43.7404966, 143.818928699999987],
-            [ -39.2036487, 148.4986786]
-        ])
+            [ -43.739252389491554, 143.818166672005248],
+            [ -39.202931919803987, 148.499155371827044]
+        ], {padding: [4,4]})
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
